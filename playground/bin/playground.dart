45c77ecab7f9506794f386bf30b304ce983f509e
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-12-09 21:26:47 +0100

    update

--- playground/bin/playground.dart
+++ playground/bin/playground.dart
@@ -6,8 +6,6 @@ class Solution {
   List<int> productExceptSelf(List<int> nums) {
     List<int> result = List<int>.generate(nums.length, (_) => 1);
 
-    print(result);
-
     int prefixInt = 1;
     for (var i = 0; i < nums.length; i++) {
       result[i] *= prefixInt;
